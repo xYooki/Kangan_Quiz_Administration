421d4079b1374968c11b8cd22bd3463e11f05d3c
xYooki <yukitheegg@gmail.com>
2023-10-25 11:26:07 +1100

    Added diff svelte files for each section

---
+++ kangan_quiz_administration/src/routes/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { Router, Link } from "svelte-routing";
+    import Login from "./Login.svelte";
+    import QuizManager from "./QuizManager.svelte";
+  </script>
+  
+  <Router>
+    <Login path="/" />
+    <QuizManager path="/quiz-manager" />
+  </Router>
