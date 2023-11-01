55cb779e5594f719fb7b839cf226c090a5b3a9c8
xYooki <yukitheegg@gmail.com>
2023-11-01 15:01:07 +1100

    Post->GET

--- kangan_quiz_administration/src/routes/Login.svelte
+++ kangan_quiz_administration/src/routes/Login.svelte
@@ -17,7 +17,7 @@
   async function handleLogin() {
     try {
       const response = await fetch("https://kanganquizapi1.azurewebsites.net/users/{username}", { //api link goes hereee whenever its ready
-        method: "POST", // Or "GET" depending on your API endpoint
+        method: "GET", // Or "GET" depending on your API endpoint
         headers: {
           "Content-Type": "application/json",
         },
